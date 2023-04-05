f30320bac017f3f2e15e9c21356aa1059e97f28e
Mk <minkhant14616@gmail.com>
2023-04-05 13:57:44 +0630

    create and edit fixed

--- resources/views/blog/create.blade.php
+++ resources/views/blog/create.blade.php
@@ -18,73 +18,83 @@
 
         </x-card-header>
 
-        <div class="flex flex-warp gap-x-8 justify-evenly">
-
-            <div class="md:w-2/3 lg:w-1/2">
-                <form class="max-w lg mx-auto" action="{{ route('blog.store') }}" method="post">
-                    @csrf
-                    <div class="form-control w-full">
-                        <label class="label font-bold text-xl" for="Description">Description</label>
-                        <textarea name="description" class="textarea textarea-bordered textarea-lg w-full max-w-xl"
-                            placeholder="Enter Blog Description here" cols="30" rows="15"></textarea>
-                    </div>
+        <form action="{{ route('blog.store') }}" method="post">
+            @csrf
+            <div class="grid grid-cols-2 gap-5">
+                <div class="form-control col-span-1">
+                    <label class="label">
+                        <span class="label-text font-bold text-xl">Description</span>
+                    </label>
+                    <textarea name="description" class="textarea textarea-bordered textarea-lg w-full max-w-xl"
+                        placeholder="Enter Blog Description here" cols="30" rows="15"></textarea>
                     @error('description')
                         <div class="text-red-500">{{ $message }}</div>
                     @enderror
+                </div>
+
+                <div class=class="col-span-1">
+                    <div class="form-control col-span-1 mb-2">
+                        <label class="label">
+                            <span class="label-text font-bold text-xl">Title</span>
+                        </label>
+                        <input type="text" placeholder="Enter Blog Title here"
+                            class="input input-bordered input-md w-full max" name="title">
+                        @error('title')
+                            <div class="text-red-500">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <div class="form-control col-span-1 mb-2">
+                        <label class="label">
+                            <span class="label-text font-bold text-xl">Choose Category</span>
+                        </label>
+                        <select class="select select-bordered select-md w-full max" name="category">
+                            @forelse ($categories as $category)
+                                <option value="{{ $category->id }}">
+                                    {{ $category->title }}
+                                </option>
+                            @empty
+                            @endforelse
+                        </select>
+                        @error('category')
+                            <div class="text-red-500">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <div class="form-control col-span-1 mb-2">
+                        <label class="label">
+                            <span class="label-text font-bold text-xl">Written by</span>
+                        </label>
+                        <input type="text" placeholder="Enter Authour Name here"
+                            class="input input-bordered input-md w-full max" name="author_name">
+                        @error('author_name')
+                            <div class="text-red-500">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <div class="form-control col-span-1 mb-2">
+                        <label class="label">
+                            <span class="label-text font-bold text-xl">Design by</span>
+                        </label>
+                        <input type="text" placeholder="Enter Designer Name here"
+                            class="input input-bordered input-md w-full max" name="designer_name">
+                        @error('designer_name')
+                            <div class="text-red-500">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <div class="form-control col-span-1 mb-2">
+                        <label class="label">
+                            <span class="label-text font-bold text-xl">Image</span>
+                        </label>
+                        <input type="text" placeholder="Enter Image Url"
+                            class="file-input file-input-primary file-input-bordered w-full max" name="image">
+                        @error('image')
+                            <div class="text-red-500">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <button type="submit" class="btn btn-primary mt-2">
+                        <i class="fa-solid fa-circle-plus mr-3"></i> Create
+                    </button>
+                </div>
             </div>
-            <div>
-                <div class="form-control w-full">
-                    <label class="label font-bold text-xl" for="title">Title</label>
-                    <input type="text" placeholder="Enter Blog Title here"
-                        class="input input-bordered input-md w-full max" name="title">
-                    @error('title')
-                        <div class="text-red-500">{{ $message }}</div>
-                    @enderror
-                </div>
-                <div class="form-control w-full">
-                    <label class="label font-bold text-xl" for="category">Choose Category</label>
-                    <select class="select select-bordered select-md w-full max" name="category">
-                        @forelse ($categories as $category)
-                            <option value="{{ $category->id }}">
-                                {{ $category->title }}
-                            </option>
-                        @empty
-                        @endforelse
-                    </select>
-                    @error('category')
-                        <div class="text-red-500">{{ $message }}</div>
-                    @enderror
-                </div>
-                <div class="form-control w-full">
-                    <label class="label font-bold text-xl" for="author_name">Written by</label>
-                    <input type="text" placeholder="Enter Authour Name here"
-                        class="input input-bordered input-md w-full max" name="author_name">
-                    @error('author_name')
-                        <div class="text-red-500">{{ $message }}</div>
-                    @enderror
-                </div>
-                <div class="form-control w-full">
-                    <label class="label font-bold text-xl" for="designer_name">Design by</label>
-                    <input type="text" placeholder="Enter Designer Name here"
-                        class="input input-bordered input-md w-full max" name="designer_name">
-                    @error('designer_name')
-                        <div class="text-red-500">{{ $message }}</div>
-                    @enderror
-                </div>
-                <div class="form-control w-full">
-                    <label class="label font-bold text-xl" for="image">Image</label>
-                    <input type="text" placeholder="Enter Image Url"
-                        class="file-input file-input-primary file-input-bordered w-full max" name="image">
-                    @error('image')
-                        <div class="text-red-500">{{ $message }}</div>
-                    @enderror
-                </div>
-                <button type="submit" class="btn btn-primary mt-2">
-                    <i class="fa-solid fa-circle-plus mr-3"></i> Create
-                </button>
-            </div>
-            </form>
-        </div>
+        </form>
 
     </x-card>
 @endsection
--- resources/views/blog/edit.blade.php
+++ resources/views/blog/edit.blade.php
@@ -18,85 +18,93 @@
 
         </x-card-header>
 
-        <div class="flex flex-wrap gap-x-8 justify-evenly">
+        <form class="max-w lg mx-auto" action="{{ route('blog.update', $blog->id) }}" method="post">
+            @csrf
+            @method('put')
+            <div class="grid grid-cols-2 gap-5">
+                <div class="form-control col-span-1">
+                    <label class="label">
+                        <span class="label-text font-bold text-xl">Description</span>
+                    </label>
+                    <textarea name="description" class="textarea textarea-bordered textarea-lg w-full max-w-xl" cols="30"
+                        rows="16">{{ old('description', $blog->description) }}</textarea>
+                    @error('description')
+                        <div class="text-red-500">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class=class="col-span-1">
+                    <div class="form-control col-span-1">
+                        <label class="">
+                            <span class="label-text font-bold text-xl">Old Photo</span>
+                        </label>
 
-            <div class="md:w-2/3 lg:w-1/2">
-                <form class="max-w lg mx-auto" action="{{ route('blog.update', $blog->id) }}" method="post">
-                    @csrf
-                    @method('put')
-                    <div class="form-control w-full">
-                        <label class="label font-bold text-xl" for="Description">Description</label>
-                        <textarea name="description" class="textarea textarea-bordered textarea-lg w-full max-w-xl" cols="30"
-                            rows="16">{{ old('description', $blog->description) }}</textarea>
-                        @error('description')
+                        <img src="{{ $blog->image }}" class="w-40 rounded" alt="">
+                    </div>
+                    <div class="form-control col-span-1 mb-2">
+                        <label class="label">
+                            <span class="label-text font-bold text-xl">Title</span>
+                        </label>
+                        <input type="text" class="input input-bordered input-md w-full max" name="title"
+                            value="{{ old('title', $blog->title) }}">
+                        @error('title')
                             <div class="text-red-500">{{ $message }}</div>
                         @enderror
                     </div>
-            </div>
-            <div>
-                <div class="form-control w-full">
-                    <label class="label font-bold text-xl" for="title">Title</label>
-                    <input type="text" class="input input-bordered input-md w-full max" name="title"
-                        value="{{ old('title', $blog->title) }}">
-                    @error('title')
-                        <div class="text-red-500">{{ $message }}</div>
-                    @enderror
-                </div>
-                <div class="form-control w-full">
-                    <label class="label font-bold text-xl" for="category">Choose Category</label>
-                    <select class="select select-bordered select-md w-full max" @error('category') is-invalid @enderror"
-                        name="category">
-                        @forelse ($categories as $category)
-                            <option value="{{ $category->id }}"
-                                {{ $category->id === $blog->category_id ? 'selected' : '' }}>
-                                {{ $category->title }}
-                            </option>
-                        @empty
-                        @endforelse
-                    </select>
-                    @error('category')
-                        <div class="text-red-500">{{ $message }}</div>
-                    @enderror
-                </div>
-                <div class="form-control w-full">
-                    <label class="label font-bold text-xl" for="author_name">Written by</label>
-                    <input type="text" class="input input-bordered input-md w-full max" name="author_name"
-                        value="{{ old('author_name', $blog->author_name) }}">
-                    @error('author_name')
-                        <div class="text-red-500">{{ $message }}</div>
-                    @enderror
-                </div>
-                <div class="form-control w-full">
-                    <label class="label font-bold text-xl" for="designer_name">Design by</label>
-                    <input type="text" class="input input-bordered input-md w-full max" name="designer_name"
-                        value="{{ old('designer_name', $blog->designer_name) }}">
-                    @error('designer_name')
-                        <div class="text-red-500">{{ $message }}</div>
-                    @enderror
-                </div>
-                <div class="form-control col-span-1">
-                    <label class="">
-                        <span class="label-text font-bold text-xl">Old Photo</span>
-                    </label>
+                    <div class="form-control col-span-1 mb-2">
+                        <label class="label">
+                            <span class="label-text font-bold text-xl">Choose Category</span>
+                        </label>
+                        <select class="select select-bordered select-md w-full max" @error('category') is-invalid @enderror"
+                            name="category">
+                            @forelse ($categories as $category)
+                                <option value="{{ $category->id }}"
+                                    {{ $category->id === $blog->category_id ? 'selected' : '' }}>
+                                    {{ $category->title }}
+                                </option>
+                            @empty
+                            @endforelse
+                        </select>
+                        @error('category')
+                            <div class="text-red-500">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <div class="form-control col-span-1 mb-2">
+                        <label class="label">
+                            <span class="label-text font-bold text-xl">Written by</span>
+                        </label>
+                        <input type="text" class="input input-bordered input-md w-full max" name="author_name"
+                            value="{{ old('author_name', $blog->author_name) }}">
+                        @error('author_name')
+                            <div class="text-red-500">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <div class="form-control col-span-1 mb-2">
+                        <label class="label">
+                            <span class="label-text font-bold text-xl">Design by</span>
+                        </label>
+                        <input type="text" class="input input-bordered input-md w-full max" name="designer_name"
+                            value="{{ old('designer_name', $blog->designer_name) }}">
+                        @error('designer_name')
+                            <div class="text-red-500">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <div class="form-control col-span-1 mb-2">
+                        <label class="label">
+                            <span class="label-text font-bold text-xl">Image</span>
+                        </label>
+                        <input type="text" class="file-input file-input-primary file-input-bordered w-full max"
+                            name="image" value="{{ old('image', $blog->image) }}">
+                        @error('image')
+                            <div class="text-red-500">{{ $message }}</div>
+                        @enderror
+                    </div>
 
-                    <img src="{{ $blog->image }}" class="w-40 rounded" alt="">
-                </div>
-                <div class="form-control w-full">
-                    <label class="label font-bold text-xl" for="Image">Image</label>
-                    <input type="text" class="file-input file-input-primary file-input-bordered w-full max"
-                        name="image" value="{{ old('image', $blog->image) }}">
-                    @error('image')
-                        <div class="text-red-500">{{ $message }}</div>
-                    @enderror
-                </div>
-                <div>
                     <button type="submit" class="btn btn-primary mt-2">
                         <i class="fa-solid fa-arrow-up-from-bracket mr-3"></i> Update
                     </button>
                 </div>
-                </form>
             </div>
-        </div>
+        </form>
 
     </x-card>
 @endsection
