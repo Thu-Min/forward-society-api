2af2765af58dac13fcb1eb5ac3482eea43100909
Thet Htoo Aung <ththtooaung.e@gmail.com>
2023-05-24 10:20:23 +0630

    EventResource & EventShowResource

--- app/Http/Controllers/ApiEventController.php
+++ app/Http/Controllers/ApiEventController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\EventResource;
+use App\Http\Resources\EventShowResource;
 use Illuminate\Http\Request;
 use App\Models\Event;
 
@@ -19,6 +20,6 @@ class ApiEventController extends Controller
 
     public function show(Event $event)
     {
-        return new EventResource($event);
+        return new EventShowResource($event);
     }
 }
